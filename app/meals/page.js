2fe702a2a71445792ce491dcf7a0bd995298aaf4
Kareem Qasrawi <kareemalqasrawi@gmail.com>
2025-12-14 16:18:57 +0300

    add meals grid list and add item for eash meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,21 @@
 import React from "react";
+import classess from "./page.module.css"
+import Link from "next/link";
+import MealsGrid from "@/comonents/meals/meals-grid";
 
 function MealsePage() {
-  return <h1>Mealse Page</h1>;
+  return <>
+    <header className={classess.header}>
+      <h1>Delichious meals, created <span className={classess.highlight}>by you</span></h1>
+      <p>Choose your favorite recipe and cook it yoursefl. it is easy and fun!</p>
+      <p className={classess.cta}>
+        <Link href="/meals/share">Shrae Your Favority Recipe</Link>
+      </p>
+    </header>
+    <main className={classess.main}>
+      <MealsGrid meals={[]}/>
+    </main>
+  </>
 }
 
 export default MealsePage;
