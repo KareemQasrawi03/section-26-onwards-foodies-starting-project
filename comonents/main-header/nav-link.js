a49a58b6c5ccf33ab721b33f7b5025ee5fbcccf5
Kareem Qasrawi <kareemalqasrawi@gmail.com>
2025-12-14 15:05:50 +0300

    create nav link custome component and usePathename to condtion active or not link and add css file

---
+++ comonents/main-header/nav-link.js
@@ -0,0 +1,21 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import classes from "./nav-link.module.css";
+
+function NavLink({ href, children }) {
+  const pathName = usePathname();
+  return (
+    <Link
+      href={href}
+      className={
+        pathName === href ? `${classes.link} ${classes.active}` : classes.link
+      }
+    >
+      {children}
+    </Link>
+  );
+}
+
+export default NavLink;
