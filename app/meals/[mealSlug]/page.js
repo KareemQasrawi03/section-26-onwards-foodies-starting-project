52155518ad1cf2d5ddd5a820fc5705f750584da3
Kareem Qasrawi <kareemalqasrawi@gmail.com>
2025-12-15 14:19:04 +0300

    add not-found,error,loading and meal item

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import clasess from "./page.module.css";
+import Image from "next/image";
+import getMeal from "@/lib/meals";
 
-function MealDetailsPage() {
+function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
   return (
-    <h1>
-    Meals Details
-      
-    </h1>
-  )
+    <>
+      <header className={clasess.header}>
+        <div className={clasess.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={clasess.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={clasess.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={clasess.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={clasess.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
 
 export default MealDetailsPage;
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,21 +1,38 @@
-import React from "react";
-import classess from "./page.module.css"
+import React, { Suspense } from "react";
+import classess from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/comonents/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-function MealsePage() {
-  return <>
-    <header className={classess.header}>
-      <h1>Delichious meals, created <span className={classess.highlight}>by you</span></h1>
-      <p>Choose your favorite recipe and cook it yoursefl. it is easy and fun!</p>
-      <p className={classess.cta}>
-        <Link href="/meals/share">Shrae Your Favority Recipe</Link>
-      </p>
-    </header>
-    <main className={classess.main}>
-      <MealsGrid meals={[]}/>
-    </main>
-  </>
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+async function MealsePage() {
+  return (
+    <>
+      <header className={classess.header}>
+        <h1>
+          Delichious meals, created{" "}
+          <span className={classess.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yoursefl. it is easy and fun!
+        </p>
+        <p className={classess.cta}>
+          <Link href="/meals/share">Shrae Your Favority Recipe</Link>
+        </p>
+      </header>
+      <main className={classess.main}>
+        <Suspense
+          fallback={<p className={classess.loading}>Fetching Meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 }
 
 export default MealsePage;
